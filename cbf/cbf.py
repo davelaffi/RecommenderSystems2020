27b4c62088d86fb85bfd00d34ca658be005548c5
Dave9797 <50965183+Dave9797@users.noreply.github.com>
2020-11-29 10:53:40 +0100

    Added cbf directory and the file "Hybrid CBF all" with the code

---
+++ cbf/cbf.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+from Compute_Similarity_Python import Compute_Similarity_Python
+
+
+class ContentBasedFiltering(object):
+
+    def __init__(self, knn=100, shrink=2):
+        self.knn = knn
+        self.shrink = shrink
+
+    def compute_similarity(self, ICM, knn, shrink):
+        similarity_object = Compute_Similarity_Python(ICM.transpose(), topK=knn, shrink=shrink,
+                                                     normalize=True, similarity="cosine")
+        return similarity_object.compute_similarity()
+
+    def fit(self, URM, ICM):
+
+        self.URM = URM
+        self.ICM = ICM
+
+        self.SM = self.compute_similarity(self.ICM, self.knn, self.shrink)
+
+    def get_expected_ratings(self, user_id):
+        
+        items_all = self.URM[user_id] #URM_all
+        expected_ratings_all_items = items_all.dot(self.SM).toarray().ravel()
+        
+        return expected_ratings_all_items
+
+    def recommend(self, user_id, urm_train: sp.csr_matrix, at):
+        
+        expected_ratings_all_items = self.get_expected_ratings(user_id)
+
+        recommended_items = np.flip(np.argsort(expected_ratings_all_items), 0)
+        return recommended_items[0:at]
