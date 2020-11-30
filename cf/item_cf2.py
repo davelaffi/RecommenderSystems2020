59efae56330ab2c153e48276dbd2352436e127aa
Dave9797 <50965183+Dave9797@users.noreply.github.com>
2020-11-30 20:07:58 +0100

    Sperem

--- cf/item_cf2.py
+++ cf/item_cf2.py
@@ -8,36 +8,123 @@ class ItemBasedCollaborativeFiltering(object):
     RECOMMENDER_NAME = "ItemBasedCFRecommender"
 
     def __init__(self,URM):
-        self.URM = URM
-        self.SM_item = None
+        self.URM_train = URM
+        self.W_sparse = None
 
     def generate_similarity_matrix(self):
-        similarity_object = Compute_Similarity_Python(self.URM, topK =self.knn, shrink = self.shrink, normalize=True, similarity=self.similarity)
+        similarity_object = Compute_Similarity_Python(self.URM_train, topK =self.knn, shrink = self.shrink, normalize=True, similarity=self.similarity)
         return similarity_object.compute_similarity()
 
-    def fit(self,knn = 100, shrink = 5, similarity="cosine"):
-        self.URM = self.URM.tocsr()
+    def fit(self,knn, shrink, similarity):
+        self.URM_train = self.URM_train.tocsr()
         self.knn = knn
         self.shrink = shrink
         self.similarity = similarity
-        self.SM_item = self.generate_similarity_matrix()
-        self.RECS = self.URM.dot(self.SM_item)
+        self.W_sparse = self.generate_similarity_matrix()
+        self.RECS = self.URM_train.dot(self.W_sparse)
 
     def get_expected_ratings(self,user_id):
         expected_ratings = self.RECS[user_id].todense()
         return np.squeeze(np.asarray(expected_ratings))
 
-    def recommend(self, user_id,remove_seen_flag, at=10):
-        expected_ratings = self.get_expected_ratings(user_id)
-        recommended_items = np.flip(np.argsort(expected_ratings), 0)
-        
-        if remove_seen_flag:
-           unseen_items_mask = np.in1d(recommended_items,self.URM[user_id].indices,
-                                        assume_unique=True, invert=True)
-           recommended_items = recommended_items[unseen_items_mask]
+    def _compute_item_score(self, user_id_array, items_to_compute=None):
+        """
+        URM_train and W_sparse must have the same format, CSR
+        :param user_id_array:
+        :param items_to_compute:
+        :return:
+        """
 
-           
-        return recommended_items[0:at]
+        user_profile_array = self.URM_train[user_id_array]
+
+        if items_to_compute is not None:
+            item_scores = - np.ones((len(user_id_array), self.URM_train.shape[1]), dtype=np.float32)*np.inf
+            item_scores_all = user_profile_array.dot(self.W_sparse).toarray()
+            item_scores[:, items_to_compute] = item_scores_all[:, items_to_compute]
+        else:
+            item_scores = user_profile_array.dot(self.W_sparse).toarray()
+
+        return item_scores
+    
+    def _remove_seen_on_scores(self, user_id, scores):
+
+        assert self.URM_train.getformat() == "csr", "Recommender_Base_Class: URM_train is not CSR, this will cause errors in filtering seen items"
+
+        seen = self.URM_train.indices[self.URM_train.indptr[user_id]:self.URM_train.indptr[user_id + 1]]
+
+        scores[seen] = -np.inf
+        return scores
+
+    def recommend(self, user_id_array, cutoff=10, remove_seen_flag=True, items_to_compute = None, remove_top_pop_flag=False, remove_custom_items_flag=False, return_scores=True):
+        
+       # If is a scalar transform it in a 1-cell array
+        if np.isscalar(user_id_array):
+            user_id_array = np.atleast_1d(user_id_array)
+            single_user = True
+        else:
+            single_user = False
+
+        if cutoff is None:
+            cutoff = self.URM_train.shape[1] - 1
+
+        # Compute the scores using the model-specific function
+        # Vectorize over all users in user_id_array
+        scores_batch = self._compute_item_score(user_id_array, items_to_compute=items_to_compute)
+
+
+        for user_index in range(len(user_id_array)):
+
+            user_id = user_id_array[user_index]
+
+            if remove_seen_flag:
+                scores_batch[user_index,:] = self._remove_seen_on_scores(user_id, scores_batch[user_index, :])
+
+            # Sorting is done in three steps. Faster then plain np.argsort for higher number of items
+            # - Partition the data to extract the set of relevant items
+            # - Sort only the relevant items
+            # - Get the original item index
+            # relevant_items_partition = (-scores_user).argpartition(cutoff)[0:cutoff]
+            # relevant_items_partition_sorting = np.argsort(-scores_user[relevant_items_partition])
+            # ranking = relevant_items_partition[relevant_items_partition_sorting]
+            #
+            # ranking_list.append(ranking)
+
+
+        # relevant_items_partition is block_size x cutoff
+        relevant_items_partition = (-scores_batch).argpartition(cutoff, axis=1)[:,0:cutoff]
+
+        # Get original value and sort it
+        # [:, None] adds 1 dimension to the array, from (block_size,) to (block_size,1)
+        # This is done to correctly get scores_batch value as [row, relevant_items_partition[row,:]]
+        relevant_items_partition_original_value = scores_batch[np.arange(scores_batch.shape[0])[:, None], relevant_items_partition]
+        relevant_items_partition_sorting = np.argsort(-relevant_items_partition_original_value, axis=1)
+        ranking = relevant_items_partition[np.arange(relevant_items_partition.shape[0])[:, None], relevant_items_partition_sorting]
+
+        ranking_list = [None] * ranking.shape[0]
+
+        # Remove from the recommendation list any item that has a -inf score
+        # Since -inf is a flag to indicate an item to remove
+        for user_index in range(len(user_id_array)):
+            user_recommendation_list = ranking[user_index]
+            user_item_scores = scores_batch[user_index, user_recommendation_list]
+
+            not_inf_scores_mask = np.logical_not(np.isinf(user_item_scores))
+
+            user_recommendation_list = user_recommendation_list[not_inf_scores_mask]
+            ranking_list[user_index] = user_recommendation_list.tolist()
+
+
+
+        # Return single list for one user, instead of list of lists
+        if single_user:
+            ranking_list = ranking_list[0]
+
+
+        if return_scores:
+            return ranking_list, scores_batch
+
+        else:
+            return ranking_list
     
     def get_URM_train(self):
-        return self.URM
+        return self.URM_train
